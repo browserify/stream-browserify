428bcc92823739ea5a11a8755cca32b8f20a3858
Renée Kooi <renee@kooi.me>
2020-01-23 14:46:55 +0100

    that is not how that works

--- test/index.js
+++ test/index.js
@@ -1,5 +1,2 @@
-var internalStreamPath = require.resolve('readable-stream/lib/internal/streams/stream');
-require.resolve[internalStreamPath] = require('readable-stream/lib/internal/streams/stream-browser');
-
 require('./buf');
 require('./pipeline');
