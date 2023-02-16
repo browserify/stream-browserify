4528a89a6594c9555ce844348c39a078ff67ab84
L. Sousa <lm.sousa@fe.up.pt>
2023-02-16 17:25:25 +0000

    Bump dependency versions
    Fixes a number of security vulnerabilities.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ Stream.Duplex = require('readable-stream/lib/_stream_duplex.js');
 Stream.Transform = require('readable-stream/lib/_stream_transform.js');
 Stream.PassThrough = require('readable-stream/lib/_stream_passthrough.js');
 Stream.finished = require('readable-stream/lib/internal/streams/end-of-stream.js')
-Stream.pipeline = require('readable-stream/lib/internal/streams/pipeline.js')
+Stream.pipeline = require('readable-stream/lib/internal/streams/pipeline.js').pipeline
 
 // Backwards-compat with node 0.4.x
 Stream.Stream = Stream;
